a1e0a6f77db204b7322ab8cf6689287ef73bb957
rapzaDev <rapzadev@gmail.com>
2022-10-24 16:27:02 -0300

    Criaçao de tema global e definiçao de tipos do styled components com typescript

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import { View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import Dashboard from './src/screens/Dashboard/dashboard.screen'
 import { useStickyImmersiveReset } from './statusBar'
+import { ThemeProvider } from 'styled-components/native'
+import theme from './src/global/styles/theme'
 
 export default function App() {
   useStickyImmersiveReset()
@@ -11,7 +13,9 @@ export default function App() {
     <View style={{ flex: 1 }}>
       <StatusBar style="dark" hidden={false} />
 
-      <Dashboard />
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
     </View>
   )
 }
--- src/screens/Dashboard/dashboard.styles.ts
+++ src/screens/Dashboard/dashboard.styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #999;
+  background-color: ${({ theme }) => theme.colors.background};
 `
 
 export const Title = styled.Text``
