f09c53eb76c9fc7f50d90df97864709d23857aa7
rapzaDev <rapzadev@gmail.com>
2022-10-31 19:01:35 -0300

    style: Estilizaçao do react bottom tab navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Platform } from 'react-native'
+import { useTheme } from 'styled-components'
+import { MaterialIcons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Dashboard from '../screens/Dashboard/dashboard.screen'
 import Register from '../screens/Register/register.screen'
@@ -6,11 +9,53 @@ import Register from '../screens/Register/register.screen'
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export default function ScreenRoutes() {
+  const theme = useTheme()
+
   return (
-    <Navigator>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
-      <Screen name="Resumo" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 60,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          paddingBottom: 0,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   )
 }
